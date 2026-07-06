e6b37033006e20edf44896206f179b55e950acfa
Abirami R <abiramir2907@gmail.com>
2026-07-06 21:29:26 +0530

    Add files via upload

---
+++ day7/linearsearch.java
@@ -0,0 +1,25 @@
+package day7;
+
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int key = 7;
+        int a = search(arr, key);
+        if (a == -1) {
+            System.out.println("not found");
+        } else {
+            System.out.println("found");
+        }
+
+    }
+
+    static int search(int[] arr, int n) {
+        for (int num : arr) {
+            if (num == n) {
+                return 1;
+            }
+
+        }
+        return -1;
+    }
+}
