d80cb58b7a45fe004095896ff24db7a2269ed088
Abirami R <abiramir2907@gmail.com>
2026-07-06 12:02:09 +0530

    Add files via upload

---
+++ day2/ascii.java
@@ -0,0 +1,11 @@
+package day2;
+import java.util.Scanner;
+public class ascii {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter No:");
+        int a=sc.nextInt();
+        char b=(char) a;
+        System.out.println(b);
+    }
+}
