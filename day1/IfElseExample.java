e48d5fff85d741044bc7e5da8d75a7fc01b16bb1
Abirami R <abiramir2907@gmail.com>
2026-07-06 13:07:41 +0530

    Add files via upload

---
+++ day1/IfElseExample.java
@@ -0,0 +1,12 @@
+package day1;
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Positive Number");
+        } else {
+            System.out.println("Negative or Zero");
+        }
+    }
+}
